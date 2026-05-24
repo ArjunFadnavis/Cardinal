bc1d01b493122c147a325969c761f49f4ed06094
afadnav2 <afadnav2@ncsu.edu>
2026-05-24 08:12:44 -0400

    Updated post run script

--- postRunScript.sh
+++ postRunScript.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 mvn clean package
-rm target/boat-rental-0.1.0.jar.original
-mv target/boat-rental-0.1.0.jar Cardinal.jar
+cd target
+rm boat-rental-0.1.0.jar.original
+mv boat-rental-0.1.0.jar Cardinal.jar
+cd ../
 git add .
 read -p "Enter your commit message: " message
 git commit -m "$message"
